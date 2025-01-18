37def1c9c9e86785dc5b32c8161dbfd0d8490464
freqnik <freqnik@protonmail.com>
2025-01-18 17:49:32 -0500

    Add limit to get-delegators.sh

--- get-delegators.sh
+++ get-delegators.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 VALIDATOR_ADDRESS="jklvaloper1wq34uv9rhkv7tjk0pu263cfyshhgr9frn2ztds"
+LIMIT=1000
 
-~/canined query staking delegations-to $VALIDATOR_ADDRESS --output json | jq -r '.delegation_responses[] | "\(.delegation.delegator_address),\(.delegation.shares)"' | cut -d "." -f 1
+~/canined query staking delegations-to $VALIDATOR_ADDRESS --limit $LIMIT --output json | jq -r '.delegation_responses[] | "\(.delegation.delegator_address),\(.delegation.shares)"' | cut -d "." -f 1
